c027f0bac1d6a85905a1dc11213c31b038d2dabe
Arman <165942302+Arman11980@users.noreply.github.com>
2024-12-11 19:19:30 +0300

    Update introspekcia.py

--- introspekcia.py
+++ introspekcia.py
@@ -1,10 +1,18 @@
 def introspection_info(obj):
     obj_type = type(obj).__name__
-    attributes = dir(obj)
-    methods = [method for method in attributes if callable(getattr(obj, method))]
+
+    attributes = [at for at in dir(obj) if not callable(getattr(obj, at))]
+
+    methods = [m for m in dir(obj) if callable(getattr(obj, m))]
+
     module = obj.__class__.__module__
-    info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module},
-    return info
+
+    dict_int = {'type': obj_type,
+                'attributes': attributes,
+                'methods': methods,
+                'module': module}
+
+    return dict_int
 
 number_info = introspection_info(42)
 print(number_info)
